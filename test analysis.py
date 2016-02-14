b997980839f938013e2cabe32756e09046e263b1
Karel Mácha <karel.macha@karlitos.net>
2016-02-14 21:20:23 +0100

    Implements basic graphical output of the table of occurencies of the captured codes in binary format. Implements some code cleanup and the resulting figure.

--- test analysis.py
+++ test analysis.py
@@ -2,24 +2,41 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# data = {}
-#
-# with open('./captures/idle.csv', 'rb') as csvfile:
-#     reader = csv.reader(csvfile, delimiter=',')
-#
-#     for row in reader:
-#         # initialize the array if necessary
-#         data[row[0]] =  data.get(row[0],[])
-#         if 'binary' in row[1]:
-#             print row[2][2:].replace(" ", "")
-#         # insert the data
-#         data[row[0]].append(row[2])
-
-
+# Read data and count occurencies
 csv_data = pd.read_csv('./captures/idle binary.csv')
 
 occurencies = csv_data['Data'].value_counts().reset_index()
 occurencies.columns = ['bits', 'count']
 occurencies['stripped bits'] = occurencies['bits'].str.replace('(0b| )', '')
 
-print occurencies[['stripped bits', 'count']]
+print occurencies[['stripped bits', 'count']].values.tolist()
+
+# prepare table data
+columns = ('stripped bits', 'count')
+rows = ['%s.' % (x + 1) for x in occurencies.index]
+cells = occurencies[['stripped bits', 'count']].values.tolist()
+
+# set figure size
+# nrows, ncols = len(cells)+1, len(columns)
+# hcell, wcell = 0.3, 1.
+# hpad, wpad = 0, 0
+# fig=plt.figure(figsize=(ncols*wcell+wpad, nrows*hcell+hpad))
+
+#
+ax = plt.gca() # get current axis
+ax.axis('off')
+
+# Plot a table
+the_table = ax.table(cellText=cells,
+                      rowLabels=rows,
+                      #rowColours=colors,
+                      colLabels=columns,
+                      cellLoc='center',
+                      loc='center')
+
+#the_table.auto_set_font_size(False)
+#the_table.set_fontsize(14)
+the_table.scale(1, 2)
+plt.title('Idle - binary code')
+#plt.show()
+plt.savefig("table.png")
